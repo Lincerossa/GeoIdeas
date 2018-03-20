dfb968a379a53dc5ac81c6e8ba31fb1c8c6d6ab9
Lincerossa90 <marcello.luatti@alkemy.com>
2018-03-20 14:37:02 +0100

    [vari] piccolezze

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
-
+import { withRouter } from 'react-router-dom'
 
 import GeoPosition from './Containers/GeoPosition'
 import GuestLogin from './Containers/GuestLogin'
+import GoogleMap from './Containers/GoogleMap'
 import Layout from './Components/Layout'
 
 
@@ -13,11 +14,12 @@ import withLayout from './hoc/withLayout'
 
 const App = () => (
   <Switch>
-    <Route exact path='/' component={GuestLogin} />
-    <Route path='/map' component={withLayout(GeoPosition)} />
+    <Route exact path='/' component={withLayout(GoogleMap)} />
+    <Route path='/geoposition' component={withLayout(GeoPosition)} />
+    <Route path='/guestlogin' component={withLayout(GuestLogin)} />
   </Switch>
 )
 
 
 
-export default App
+export default withRouter(App)
