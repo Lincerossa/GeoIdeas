72baa72c50f8bf5a4b80d8550794126bf6d3935f
Lincerossa90 <marcello.luatti@alkemy.com>
2018-03-20 22:47:38 +0100

    [components] progress

--- src/Components/ModalOverlay.js
+++ src/Components/ModalOverlay.js
@@ -11,8 +11,22 @@ const modalStyle = {
   overflow: 'scroll',
   '-webkit-overflow-scrolling': 'touch',
   left: 0,
+  border: '1px solid yellow',
   right: 0,
   backgroundColor: '#000',
+  overlay: { 
+    zIndex: 1 
+  },
+  content: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    border: 'none',
+    borderRadius: 0,
+    padding: '1rem',
+  }
 }
 
 
@@ -21,6 +35,7 @@ const backdropStyle = {
   zIndex: 1040,
   top: 0,
   bottom: 0,
+  border: '1px solid red',
   overflow: 'scroll',
   '-webkit-overflow-scrolling': 'touch',
   left: 0,
@@ -50,22 +65,23 @@ class ModalOverlay extends Component {
 
     return(
 
-      <div>
-        <Modal
-          isOpen={showModal}
-          onRequestClose={closeModal}
-          style={modalStyle}
-          backdropStyle={backdropStyle}
-          contentLabel={contentLabel}
-          style={{ overlay: { zIndex: 1 } }}
+      <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        style={modalStyle}
+        backdropStyle={backdropStyle}
+        contentLabel={contentLabel}
+      >
+        <ModalContent
+          centerModal={centerModal}
         >
-          <ModalContent
-            centerModal={centerModal}
-          >
-            {children}
-          </ModalContent>
-        </Modal>
-      </div>
+          {children}
+        </ModalContent>
+        <BackWrapper onClick={closeModal}>
+          <div className="material-icons">navigate_before</div>
+          Torna indietro
+        </BackWrapper>
+      </Modal>
     )
   }
 }
@@ -75,15 +91,7 @@ const ModalContent = styled.div`
   margin: 0;
   background-color: white;
   position: relative;
-  ${props =>
-    props.centerModal
-      ? `
-      transform: translate(0,-50%);
-      top: 50%;
-    `
-      : null} &:focus {
-    outline: none;
-  }
+  margin-top: 2rem;
 
   &:after {
     content: '';
@@ -92,6 +100,18 @@ const ModalContent = styled.div`
     margin: 1.25rem;
   }
 
+`
+
+const BackWrapper = styled.div`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  letter-spacing: .1em;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-size: .5rem;
 `
 
 export default ModalOverlay
