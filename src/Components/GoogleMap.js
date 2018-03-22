7b0751537081be869c6bceb967a458ddc0c9ed66
Lincerossa90 <marcello.luatti@alkemy.com>
2018-03-22 18:50:43 +0100

    [components] clusters di markers

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import { compose, withProps } from 'recompose'
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps"
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
-const Map = ({ center, zoom, showMarker }) => {
+const MAP_KEY = 'AIzaSyCczDTBugHreC89NuyVmugAEUGKalukPAs'
+
+const Map = ({ center, zoom, showMarker, markers }) => {
 
   return(
   <GoogleMap
     zoom={zoom}
     center={center}
   >
-    {showMarker && <Marker position={center} />}
+    {
+      markers && 
+      markers.length && 
+      <MarkerClusterer
+        averageCenter
+        enableRetinaIcons
+        gridSize={60}
+      >
+        {
+          markers.map((marker, index) => (
+          <Marker
+            key={index}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />
+        ))}
+      </MarkerClusterer>
+    }
   </GoogleMap>)
 }
 
 
+
 const composedMap = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${MAP_KEY}`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100%`}} />,
     mapElement: <div style={{ height: `100%` }} />,
