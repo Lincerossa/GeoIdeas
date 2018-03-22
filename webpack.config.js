e3e7152944387fe8d0d657599deb7f746ee9e4af
Lincerossa90 <marcello.luatti@alkemy.com>
2018-03-22 16:51:59 +0100

    [package.json] babel --> stage[0]

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,6 @@ module.exports = {
         test: /\.(js||jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015'],
-        },
       },
     ],
   },
