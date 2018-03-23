1a75db0df3edf4eeb5a42f3bc46b7211dd3d5ffa
Lincerossa90 <marcello.luatti@alkemy.com>
2018-03-23 14:09:09 +0100

    [components] redux-form

---
+++ src/Components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+let Form = ({ handleSubmit, fields}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      {
+        fields &&
+        fields.map(({name, label, component="input", type="text", value}) => (
+          <div>
+            <label htmlFor={name}>{label}</label>
+            <Field name={name} component={component} type={type} value={value}/>
+          </div>
+        ))
+      }
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+Form = reduxForm({
+  // a unique name for the form
+  form: 'markerGenerator'
+})(Form)
+
+export default Form
--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -3,40 +3,90 @@ import { compose, withProps } from 'recompose'
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps"
 const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
+import ModalOverlay from './ModalOverlay'
+
 const MAP_KEY = 'AIzaSyCczDTBugHreC89NuyVmugAEUGKalukPAs'
 
-const Map = ({ center, zoom, showMarker, markers }) => {
+const Icon = () => (
+  <p>icona</p>
+)
 
-  return(
-  <GoogleMap
-    zoom={zoom}
-    center={center}
-  >
-    {
-      markers && 
-      markers.length && 
-      <MarkerClusterer
-        averageCenter
-        enableRetinaIcons
-        gridSize={60}
+
+
+class Map extends Component {
+
+
+  constructor(props){
+    super(props)
+    this.state={
+    }
+    this.handleToggleMarker = this.handleToggleMarker.bind(this)
+  }
+
+
+  handleToggleMarker(activeMarkerId) {
+    this.setState({
+      activeMarkerId
+    })
+    console.log("activeMarkerId", activeMarkerId)
+  }
+
+
+
+  render() {
+
+    const { center, zoom, showMarker, markers } = this.props
+
+    const { activeMarkerId } = this.state
+    return (
+      <GoogleMap
+        zoom={zoom}
+        center={center}
       >
         {
-          markers.map((marker, index) => (
-          <Marker
-            key={index}
-            position={{ lat: marker.lat, lng: marker.lng }}
-          />
-        ))}
-      </MarkerClusterer>
-    }
-  </GoogleMap>)
+          markers &&
+          markers.length &&
+          <MarkerClusterer
+            averageCenter
+            enableRetinaIcons
+            gridSize={60}
+          >
+            {
+              markers.map((marker, index) => (
+                <React.Fragment>
+                  <Marker
+                    key={index}
+                    onMouseOver={() => this.handleToggleMarker(index)}
+                    onTouchStart={() => this.handleToggleMarker(index)}
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                  >
+                  </Marker>
+                  { 
+                    (activeMarkerId === index) &&
+                    <ModalOverlay
+                      showModal
+                      closeModal={this.handleToggleMarker}
+                    >
+                      <p>{marker.lat}</p>
+                      <p>{marker.lng}</p>
+                      <p>{marker.address}</p>
+                      <p>{marker.category}</p>
+                      <p>{marker.description}</p>
+                    </ModalOverlay>
+                  }
+                </React.Fragment>
+              ))}
+          </MarkerClusterer>
+        }
+      </GoogleMap>)
+  }
 }
 
 
 
 const composedMap = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${MAP_KEY}`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3exp&libraries=geometry,drawing,places&key=${MAP_KEY}`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100%`}} />,
     mapElement: <div style={{ height: `100%` }} />,
